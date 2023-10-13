07d646fded00d9ecc650910c884b497e30701422
pablosm196 <pablos19@ucm.es>
2023-10-13 10:21:16 +0200

    Particle generator empezado

---
+++ skeleton/ParticleGenerator.cpp
@@ -0,0 +1,16 @@
+#include "ParticleGenerator.h"
+
+void ParticleGenerator::setParticle(Particle* model)
+{
+	_model = model;
+}
+
+GaussianParticleGenerator::GaussianParticleGenerator(Vector3 pos, Vector3 vel, double t)
+{
+
+}
+
+std::list<Particle*> GaussianParticleGenerator::generateParticles()
+{
+	return std::list<Particle*>();
+}
---
+++ skeleton/ParticleGenerator.h
@@ -0,0 +1,31 @@
+#pragma once
+#include<string>
+#include <list>
+#include "Particle.h"
+class ParticleGenerator
+{
+protected:
+	std::string _name;
+	Vector3 _mean_pos, _mean_vel;
+	double _generator_probability;
+	int _num_particles;
+	Particle* _model;
+public:
+	void setParticle(Particle* model);
+	virtual std::list<Particle*> generateParticles() = 0;
+};
+
+class GaussianParticleGenerator : public ParticleGenerator
+{
+protected:
+	Vector3 std_dev_pos, std_dev_vel;
+	double std_dev_t;
+public:
+	GaussianParticleGenerator(Vector3 pos, Vector3 vel, double t);
+	std::list<Particle*> generateParticles() override;
+};
+
+class UniformParticleGenerator : public ParticleGenerator {
+
+};
+
